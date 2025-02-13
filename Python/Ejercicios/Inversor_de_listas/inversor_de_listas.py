3522c707ff3227ec881d09e48dfb4bed682f5dd8
VictorDeniz2025 <victordenizl@gmail.com>
2025-02-13 17:13:39 +0000

    Reemplazar script de inversión de listas y agregar nueva versión con mejoras

--- Python/Ejercicios/Inversor_de_listas/inversor_de_listas.py
+++ Python/Ejercicios/Inversor_de_listas/inversor_de_listas.py
@@ -1,14 +1,13 @@
+# Este codigo invierte una lista de numeros enteros
 
 
-lista = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-lista2 = [53, 25, 36, 25, 45]
-lista = lista2
+lista = [53, 31, 36, 25, 45]
+
 rango = len(lista)
 rango2 = rango//2
 
 
 for i in range(rango2):
-
     if lista[i] > lista[rango-1-i]:
         lista[i], lista[rango-1-i] = lista[rango-1-i], lista[i]
-print(lista)
+        print(lista)
