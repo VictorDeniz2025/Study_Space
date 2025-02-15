f4fdfea997fb7f31c575827aacd1a48a7413c004
VictorDeniz2025 <victordenizl@gmail.com>
2025-02-15 17:18:17 +0000

    Actualizar el script de inversión de listas y agregar un nuevo archivo de prueba

--- Python/Ejercicios/Inversor_de_listas/inversor_de_listas.py
+++ Python/Ejercicios/Inversor_de_listas/inversor_de_listas.py
@@ -1,13 +1,16 @@
 # Este codigo invierte una lista de numeros enteros
+# Toma la lista y la invierte, es decir, el primer elemento pasa a ser el ultimo y el ultimo el primero
 
 
-lista = [53, 31, 36, 25, 45]
+lista = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(lista)
 
 rango = len(lista)
 rango2 = rango//2
 
 
 for i in range(rango2):
-    if lista[i] > lista[rango-1-i]:
-        lista[i], lista[rango-1-i] = lista[rango-1-i], lista[i]
-        print(lista)
+    lista[i], lista[rango-1-i] = lista[rango-1-i], lista[i]
+
+print(lista)
---
+++ Python/Ejercicios/Pruebas.py/prueba.py
@@ -0,0 +1,8 @@
+import copy
+
+
+lista1 = [1, 2, 3, 4, 5]
+lista2 = [6, 7, 8, 9, 10]
+
+lista3 = copy.deepcopy(lista2)
+print(lista3)
