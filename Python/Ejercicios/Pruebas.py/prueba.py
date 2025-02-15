1dc6543e7e7a6682f92decbbb596b284f153d9be
VictorDeniz2025 <victordenizl@gmail.com>
2025-02-15 19:08:58 +0000

    Eliminar código comentado y simplificar la impresión de una lista invertida

--- Python/Ejercicios/Pruebas.py/prueba.py
+++ Python/Ejercicios/Pruebas.py/prueba.py
@@ -1,14 +1,3 @@
 lista = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-# Vamos a hacer un slices
-
-a = lista[1]
-b = lista[-1]
-c = lista[0:5]
-d = lista[::1]
-
-
-print(a)  # 0
-print(b)  # 8
-print(c)  # [0, 1, 2, 3, 4]
-print(d)  # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(lista[10:0:-1])
