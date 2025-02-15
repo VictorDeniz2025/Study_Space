adc01e67e3af6fc11a731c64e164d5685049fc4f
VictorDeniz2025 <victordenizl@gmail.com>
2025-02-15 17:59:23 +0000

    Actualizar pruebas y teoría sobre listas y tuplas; eliminar archivo innecesario

--- Python/Ejercicios/Pruebas.py/prueba.py
+++ Python/Ejercicios/Pruebas.py/prueba.py
@@ -1,4 +1,14 @@
-rango = range(0, 10)
-lista = list(rango)
-print(lista)
+lista = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+# Vamos a hacer un slices
+
+a = lista[1]
+b = lista[-1]
+c = lista[0:5]
+d = lista[::1]
+
+
+print(a)  # 0
+print(b)  # 8
+print(c)  # [0, 1, 2, 3, 4]
+print(d)  # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
