7e23f99cbc16a49ee92f903cea13104b0180beca
VictorDeniz2025 <victordenizl@gmail.com>
2025-03-27 18:48:29 +0000

    Agregar gráfico de dispersión y datos de ejemplo en prueba.r

--- R/prueba.r
+++ R/prueba.r
@@ -1,2 +1,10 @@
 # Código de prueba en R
 print("Hola, mundo en R")
+
+
+# Crear datos
+x <- c(1, 2, 3, 4, 5)
+y <- c(2, 4, 6, 8, 10)
+
+# Crear un gráfico de dispersión
+plot(x, y, type = "o", col = "blue", main = "Gráfico de ejemplo", xlab = "Eje X", ylab = "Eje Y")
